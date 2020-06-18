3959eec7cc94b930d5e44a327f8411eb43ee6cbb
Minchan <88minchan@gmail.com>
2020-06-18 13:08:10 +0900

    -

--- Test0617/src/main/java/com/example/test/TestController.java
+++ Test0617/src/main/java/com/example/test/TestController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
@@ -14,9 +15,51 @@ public class TestController {
 	}
 
 	@RequestMapping("/{num}")
-	public String index(@PathVariable int num, Model model) {
+	public String index1(@PathVariable int num, Model model) {
 		int res = num;
 		model.addAttribute("msg", "num : " + res);
+		return "index";
+	}
+	
+	@RequestMapping("/cal{num}/{operation}/{num1}")
+	public String index2(@PathVariable int num,@PathVariable int num1,@PathVariable String operation, Model model) {
+		int cal = 0;
+		if(operation.equals("+")) {
+			cal = num + num1;
+		}else if(operation.equals("-")) {
+			cal = num - num1;
+		}else if(operation.equals("/")) {
+			cal = num / num1;
+		}else if(operation.equals("*")) {
+			cal = num * num1;
+		}
+		model.addAttribute("msg", "num : " + cal);
+		return "index";
+	}
+	
+	
+	@RequestMapping("/add")
+	public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+		int cal = num1 + num2;
+		model.addAttribute("msg", "num : " + cal);
+		return "index";
+	}
+	@RequestMapping("/sub")
+	public String sub(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+		int cal = num1 - num2;
+		model.addAttribute("msg", "num : " + cal);
+		return "index";
+	}
+	@RequestMapping("/mul")
+	public String mul(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+		int cal = num1 * num2;
+		model.addAttribute("msg", "num : " + cal);
+		return "index";
+	}
+	@RequestMapping("/div")
+	public String div(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+		int cal = num1 / num2;
+		model.addAttribute("msg", "num : " + cal);
 		return "index";
 	}
 
